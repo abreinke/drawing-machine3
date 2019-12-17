d062a7650e48227b14aa960824fc5ced62d65041
abreinke <42782067+abreinke@users.noreply.github.com>
2019-12-16 23:46:54 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
+let cricketSound;
+function preload(){
+  soundFormats('mp3');
+  cricketSound = loadSound('crickets.mp3');
+}
+
+function setup(){
+  soundVolume = 0.5;
+  cricketSound.setVolume(soundVolume);
+}
+
 var saveCount = 0;
 function setup() {
   createCanvas(875, 500);
   background(81, 81, 81);
   stroke(253, 49, 85);
   strokeWeight(50);
+}
+
+function keyTyped(){
+  if (key == 'c'){
+  cricketSound.play();
+} else if (key == 'p') {
+  cricketSound.pause();
+}
 }
 
 function draw() {
